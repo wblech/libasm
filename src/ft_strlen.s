efabcd85fd813b80ec27554089c8d037423286e2
Wincenty Bertoni Lech <8575320+wblech@users.noreply.github.com>
2021-04-17 18:09:46 -0300

    Remove comments

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -1,4 +1,3 @@
-
 global ft_strlen
 
 ft_strlen:
--- src/ft_write.s
+++ src/ft_write.s
@@ -3,16 +3,16 @@ global ft_write
 extern __errno_location
 
 ft_write:
-	mov rax, 1 ; chama o wit do sistema
+	mov rax, 1
 	syscall
-	cmp rax, 0 ; vê se é negativo
-	jl error ; caso negativo chama o erro
+	cmp rax, 0
+	jl error
 	ret
 
 error:
-	neg rax ; pega o valor negativo e transforma em positivo
-	mov rdi, rax ; salva o valor do erro em rdi
-	call __errno_location ; chama a função error e devolverá um ponteiro para ela
-	mov [rax], rdi ; manda o valor do erro para onde a função error está lendo o erro
-	mov rax, -1 ; coloca o retorno da função ft_write
+	neg rax
+	mov rdi, rax
+	call __errno_location
+	mov [rax], rdi
+	mov rax, -1
 	ret
